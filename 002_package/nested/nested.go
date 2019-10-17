f0445d2fd8c2bdce478020f82cc158c12b3d72ca
Abaygithub <sleep_abai@mail.ru>
2019-10-17 20:36:45 +0600

    test3

--- 002_package/nested/nested.go
+++ 002_package/nested/nested.go
@@ -2,9 +2,15 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/Abaygithub/GoLangBasic/002_package/nested/hello"
+	"github.com/Abaygithub/GoLangBasic/002_package/nested/say"
 )
 
 func main() {
+
 	fmt.Println("Start program...")
+
+	fmt.Println(say.CallFromSay() + hello.CallFromHello())
 
 }
